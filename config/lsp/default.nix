eca51bcbc6f66a58753abc2ca1104b81b6874c54
Matthias Berse <matthias.berse@raith.com>
2025-01-06 08:15:35 +0100

    Enable rust_analyser and pylsp

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -6,17 +6,16 @@
       enable = true;
       servers = {
         bashls.enable = true;
-        clangd.enable = true;
-        elixirls.enable = true;
-        gleam.enable = false;
-        gopls.enable = true;
-        kotlin_language_server.enable = false;
         nixd.enable = true;
-        prolog_ls = {
-          enable = true;
-          package = pkgs.swi-prolog;
-        };
         ruff.enable = true;
+        pylsp = {
+          enable = true;
+          ruff.enabled = true;
+        };
+        rust_analyser = {
+          enable = true;
+
+        };
       };
       keymaps.lspBuf = {
         "gd" = "definition";
